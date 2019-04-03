89f42acc1afc0b82a6922f017c09f69304863e37
Кирилл Афонин <aku536@mail.ru>
2019-04-03 11:56:38 +0300

    Initial Commit

---
+++ Image Gallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Gallery
+//
+//  Created by Кирилл Афонин on 03/04/2019.
+//  Copyright © 2019 krrl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
